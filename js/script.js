548b58c056880fac5e72ff956a0499f39f9f255b
Yana Brewster <270010939@bun2-206-25.local>
2019-05-13 11:53:39 +1200

    in progress

--- js/script.js
+++ js/script.js
@@ -1,61 +1,97 @@
 //function: find max value of a number list
+// console.log("hello");
+
+
+var aNums = [0];
+// console.log(aNums);
+var iCount = [0];
+// console.log(iCount);
+var iMax = aNums;
+
+
 function findMax(aNums) {
-    var iMax = aNums[0];
+    var iMax = aNums;
+// console.log(iMax);
 
     for(var iCount=0; iCount<aNums.length; iCount++) {
         if(iMax > aNums[iCount]) {
             iMax = aNums[iCount];
         }
+        // console.log(aNums);
     }
 
     return iMax;
 }
+console.log(iMax);
+
+
+// FIRST DRAFT CODE
+
+// function findMax() {
+//     var iMax = aNums[0];
+//
+//     for(var i =0; i < aNums.length; i++) {
+//         if(iMax > aNums.length) {
+//             iMax = aNums [i];
+//         }
+//
+//     }
+//     return iMax;
+// }
+
 
 //-----------------------------------------
 
-//Input: Get 3 grades for each student
+// // //Input: Get 3 grades for each student
 var iSize = parseInt(prompt('Class size?'));
 var aGradesPhysics = [];
 var aGradesChemistry = [];
-var aGradesBiology;
+var aGradesBiology = [];
 
 //ParseInt does the same as Number but removes anything other than a number from the value if input
 //Example parseInt(abc123) will give you 123
-for(var iCount=0; iCount<iSize; iCount++) {
-    var iGradePhysics = parseInt(prompt('Student '+(iCount+1)+': Grade for Physics out of 100'));
-    GradesPhysics.push(iGradePhysics);
+for(var i=0; i < iSize; i++) {
+    var aGradesPhysics = parseInt(prompt('Student '+[i+1]+': Grade for Physics out of 100'));
+    console.log(aGradesPhysics);
+    // aGradesPhysics.push(aGradesPhysics);
+//
+console.log(aGradesPhysics);
+//
+    var aGradeChemistry = parseInt(prompt('Student '+[i+1]+': Grade for Biology out of 100'));
+    // aGradesChemistry.push(aGradesChemistry);
 
-    var iGradeChemistry = parseInt(prompt('Student '+(iCount+1)+': Grade for Biology out of 100'));
-    aGradesChemistry.push(iGradeChemistry);
-
-    var iGradeBiology = parseInt(prompt('Student '+(iCount+1)+': Grade for Chemistry out of 100'));
-    aGradesBiology.push(iGradeBiology);
+console.log(aNums);
+//
+    var iGradeBiology = parseInt(prompt('Student '+[i+1]+': Grade for Chemistry out of 100'));
+    // aGradesBiology.push(aGradesBiology);
+//
+console.log(aGradesBiology);
 }
-
-// processing: calculate average grade for each student and find top grade in class
+//
+// // // processing: calculate average grade for each student and find top grade in class
 var aAverageGrades = [];
 
-for(var iCount=0; iCount<iSize; iCount++) {
-    var fAverage = (aGradesPhysics[iCount] + aGradesChemistry[iCount] + aGradesBiology[iCount]) / aGradesPhysics.length;
-    aAverageGrades.push(fAverage);
+for(var i=0; i<iSize; i++) {
+    var fAverage = (aGradesPhysics[i] + aGradesChemistry[i] + aGradesBiology[i]) / aGradesPhysics.length;
+    // aAverageGrades.push(fAverage);
 }
 var iTopGrade = findMaxs(aAverageGrades);
 
-//output: list result for each student and top grade
+// //output: list result for each student and top grade
+//
+//
+for(var i=1; i<aAverageGrades.length; i++) {
+    if(aAverageGrades[i] < 50) {
 
+        document.write('Student '+[i+1]+': Fail'+'<br>');
 
-for(var iCount=1; iCount<aAverageGrades.length; iCount++) {
-    if(aAverageGrades[iCount] < 50) {
+    } else if(aAverageGrades[i] > 50 && aAverageGrades[i] < 80) {
 
-        document.write('Student '+(iCount+1)+': Fail'+'<br>');
-
-    } else if(aAverageGrades[iCount] > 50 && aAverageGrades[iCount] < 80) {
-
-        document.write('Student '+(iCount+1)+': Pass<br>');
+        document.write('Student '+[i+1]+': Pass<br>');
 
     } else {
 
-        document.write('Student '+(iCount+1)': Pass with Distinction'+'<br>');
+        document.write('Student '+[i+1]': Pass with Distinction'+'<br>');
     }
 }
 
