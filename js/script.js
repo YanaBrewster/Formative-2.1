2e2cb9198bb1a6ed0f43eeeafbdf1f9068b94cdd
YanaBrewster <yana.brewster@gmail.com>
2019-05-15 21:33:12 +1200

    progress

--- js/script.js
+++ js/script.js
@@ -12,12 +12,10 @@ function findMax(aNums){
     return iMax;
 }
 
-// console.log(iMax);
-// console.log(aNums);
 
 
-
-//-----------------------------------------
+//
+// -----------------------------------------
 
 //Input: Get 3 grades for each student
 var iSize = parseInt(prompt('Class size?'));
@@ -27,9 +25,10 @@ var aGradesBiology = [];
 
 //ParseInt does the same as Number but removes anything other than a number from the value if input
 //Example parseInt(abc123) will give you 123
-for(var iCount=0; iCount<iSize; iCount++) {
+for(var iCount = 0; iCount < iSize; iCount++) {
     var iGradesPhysics = parseInt(prompt('Student '+(iCount+1)+': Grade for Physics out of 100'));
     aGradesPhysics.push(iGradesPhysics);
+
     var iGradesChemistry = parseInt(prompt('Student '+(iCount+1)+': Grade for Biology out of 100'));
     aGradesChemistry.push(iGradesChemistry);
 
@@ -41,15 +40,15 @@ for(var iCount=0; iCount<iSize; iCount++) {
 var aAverageGrades = [];
 
 for(var iCount=0; iCount<iSize; iCount++) {
-    var fAverage = (aGradesPhysics[iCount] + aGradesChemistry[iCount] + aGradesBiology[iCount]) / aGradesPhysics.length;
+    var fAverage = (aGradesPhysics[iCount] + aGradesChemistry[iCount] + aGradesBiology[iCount]) / [iCount];
     aAverageGrades.push(fAverage);
 }
-var iTopGrade = findMax (aAverageGrades);
+var iTopGrade = findMax(aAverageGrades);
 
 //output: list result for each student and top grade
 
 
-for(var iCount=1; iCount<aAverageGrades.length; iCount++) {
+for(var iCount=0; iCount<aAverageGrades; iCount++) {
     if(aAverageGrades[iCount] < 50) {
 
         document.write('Student '+(iCount+1)+': Fail'+'<br>');
@@ -64,4 +63,4 @@ for(var iCount=1; iCount<aAverageGrades.length; iCount++) {
     }
 }
 
-document.write('<h1>The top overall grade is '+iTopgrade+'</h1>');
+document.write('<h1>The top overall grade is '+iTopGrade+'</h1>');
